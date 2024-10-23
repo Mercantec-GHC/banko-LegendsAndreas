7116509ffbc541385cdfe83f66ff8b170ebac502
Andreas <andreas3x3@protonmail.com>
2024-10-23 10:35:58 +0200

    færdig

--- andreas_banko/Banko/Program.cs
+++ andreas_banko/Banko/Program.cs
@@ -6,11 +6,13 @@ using System.Collections;
 
 class Program
 {
+    // Class representing the alternative bingo game
     public class AlternativeBingo : BankoPlade
     {
-        Hashtable bingoPlates = new();
-        readonly List<int> usedBingoNumbers = new();
+        Hashtable bingoPlates = new(); // Hashtable to store bingo plates by name
+        readonly List<int> usedBingoNumbers = new(); // List to keep track of used bingo numbers
 
+        // Method to start the bingo game
         public void StartGame()
         {
             Console.WriteLine("Welcome to Bingo!");
@@ -18,11 +20,12 @@ class Program
             string input = "";
             while (true)
             {
-                Console.Write("Please enter action" +
-                    "   make" +
-                    "   view" +
-                    "   insert" +
-                    "   >");
+                Console.Write("Please enter action\n" +
+                    "   make\n" +
+                    "   view one\n" +
+                    "   view all\n" +
+                    "   insert\n" +
+                    ">");
                 input = Console.ReadLine();
 
                 if (input == "make")
@@ -30,26 +33,56 @@ class Program
                     Console.Write("Enter name>");
                     CreatePlate(name: Console.ReadLine());
                 }
-                else if (input == "view")
+                else if (input == "view one")
                 {
                     Console.Write("Enter plate name>");
-                    PrintPlate(name: Console.ReadLine());
+                    try
+                    {
+                        PrintPlate(name: Console.ReadLine());
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error caught: " + ex.Message);
+                    }
+                }
+                else if (input == "view all")
+                {
+                    Console.WriteLine("All your sexy plates:");
+                    PrintAllPlates();
                 }
                 else if (input == "insert")
                 {
-
+                    Console.Write("Enter number>");
+                    try
+                    {
+                        int number = ConvertBingoNumberToInt(Console.ReadLine());
+                        InsertBingoNumber(number);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error caught: " + ex.Message);
+                    }
                 }
                 else
                 {
                     Console.WriteLine("Invalid input.");
                 }
-                // Make plate.
-                // View plate.
-                // Insert number. Check number.
             }
         }
 
-        public void CreatePlate(string name)
+        // Method to print all bingo plates
+        private void PrintAllPlates()
+        {
+            BankoPlade plade = new();
+            foreach (DictionaryEntry entry in bingoPlates)
+            {
+                plade = (BankoPlade)entry.Value;
+                plade.PrintPlade();
+            }
+        }
+
+        // Method to create a new bingo plate
+        private void CreatePlate(string name)
         {
             BankoPlade tempPlate = new();
             tempPlate.CreateRows();
@@ -57,22 +90,67 @@ class Program
             bingoPlates[name] = tempPlate;
         }
 
-        public void PrintPlate(string name)
+        // Method to print a specific bingo plate by name
+        private void PrintPlate(string name)
         {
             BankoPlade tempPlate = new();
             tempPlate = (BankoPlade)bingoPlates[name];
             tempPlate.PrintPlade();
         }
 
-        public void InsertBingoNumber()
+        // Method to convert bingo number from string to integer
+        private int ConvertBingoNumberToInt(string strNum)
         {
+            return Convert.ToInt32(strNum);
+        }
 
+        // Method to insert a bingo number and check for winners
+        private void InsertBingoNumber(int bingoNumber)
+        {
+            if (bingoNumber < 1 || bingoNumber > 90)
+            {
+                Console.WriteLine("Number is either less than 1, or more than 90.");
+                return;
+            }
+            else if (usedBingoNumbers.Contains(bingoNumber))
+            {
+                Console.WriteLine($"Number {bingoNumber} has already been said.");
+                return;
+            }
+            else
+            {
+                bool plateVictory = false;
+                string winnerName = "";
+                BankoPlade winnerPlate = new();
+                usedBingoNumbers.Add(bingoNumber);
+                foreach (DictionaryEntry entry in bingoPlates)
+                {
+                    BankoPlade plade = (BankoPlade)entry.Value;
+                    plade.CheckAndInsertPlateNumber(bingoNumber, plade);
+
+                    if (plade.GottenEntirePlate())
+                    {
+                        plateVictory = true;
+                        winnerName = plade.GetName();
+                        winnerPlate = plade;
+                        break;
+                    }
+                }
+
+                if (plateVictory)
+                {
+                    Console.WriteLine(winnerName + " Has won!");
+                    winnerPlate.PrintPlade();
+                }
+            }
         }
     }
+
+    // Class representing a basic bingo game
     public class BingoBankoSpil
     {
-        BankoPlade[] bankoPlader;
-        readonly List<int> usedBingoNumbers = new();
+        BankoPlade[] bankoPlader; // Array of bingo plates
+        readonly List<int> usedBingoNumbers = new(); // List to keep track of used bingo numbers
 
         /// <summary>
         /// Constructor for BingoBankoSpil.
@@ -92,6 +170,7 @@ class Program
             }
         }
 
+        // Method to print a bingo plate by name
         public void PrintByName(string name)
         {
             foreach (BankoPlade plade in bankoPlader)
@@ -105,6 +184,7 @@ class Program
             Console.WriteLine("Could not find plate based on name.");
         }
 
+        // Method to export bingo plates to a JSON file
         public void ExportPlatesToJSON(string filePath)
         {
             try
@@ -124,13 +204,14 @@ class Program
             }
         }
 
-
+        // Method to check for errors in bingo plates
         public void ErrorCheckingPlates()
         {
             foreach (var plade in bankoPlader)
                 plade.ErrorCheckingRows();
         }
 
+        // Method to simulate the game and determine the winner
         public void ResolvePlates()
         {
             bool plateVictory = false;
@@ -141,7 +222,7 @@ class Program
                 int bingoNumber = GetBingoNumber();
                 foreach (BankoPlade plade in bankoPlader)
                 {
-                    plade.CheckPlateNumber(bingoNumber, plade);
+                    plade.CheckAndInsertPlateNumber(bingoNumber, plade);
 
                     if (plade.GottenEntirePlate())
                     {
@@ -161,6 +242,7 @@ class Program
             }
         }
 
+        // Method to get a random bingo number
         private int GetBingoNumber()
         {
             Random randCaller = new Random();
@@ -179,12 +261,14 @@ class Program
             }
         }
 
+        // Method to print all bingo plates in the game
         public void PrintGame()
         {
             foreach (BankoPlade plade in bankoPlader)
                 plade.PrintPlade();
         }
 
+        // Method to print a bingo plate by ID
         public void PrintById(int idNum)
         {
             foreach (BankoPlade plade in bankoPlader)
@@ -194,38 +278,45 @@ class Program
             }
         }
     }
+
+    // Class representing an individual bingo plate
     public class BankoPlade
     {
-        public string? name { get; set; }
-        public int? id { get; set; }
-        public int earnedRows { get; set; } = 0;
-        public Row Row1 { get; set; } = new();
-        public Row Row2 { get; set; } = new();
-        public Row Row3 { get; set; } = new();
+        // Properties of a bingo plate
+        public string? name { get; set; } // Name of the plate
+        public int? id { get; set; } // ID of the plate
+        public int earnedRows { get; set; } = 0; // Number of rows earned (marked)
+        public Row row1 { get; set; } = new(); // First row of the plate
+        public Row row2 { get; set; } = new(); // Second row of the plate
+        public Row row3 { get; set; } = new(); // Third row of the plate
 
+        // Method to assign an ID to the plate
         public void AssignID(int idNum)
         {
             id = idNum;
         }
 
+        // Method to get the name of the plate
         public string GetName()
         {
             return name;
         }
 
+        // Method to check if the entire plate is completed
         public bool GottenEntirePlate()
         {
-            if (Row1.lineStatus == true && Row2.lineStatus == true && Row3.lineStatus == true)
+            if (row1.lineStatus == true && row2.lineStatus == true && row3.lineStatus == true)
                 return true;
             else
                 return false;
         }
 
+        // Method to check for errors in the rows of the plate
         public void ErrorCheckingRows()
         {
-            int rowOneCounter = Row1.ExamineRow();
-            int rowTwoCounter = Row2.ExamineRow();
-            int rowThreeCounter = Row3.ExamineRow();
+            int rowOneCounter = row1.ExamineRow();
+            int rowTwoCounter = row2.ExamineRow();
+            int rowThreeCounter = row3.ExamineRow();
 
             if (rowOneCounter != 4)
                 Console.WriteLine("Error at row 1");
@@ -237,43 +328,47 @@ class Program
                 Console.WriteLine("We good");
         }
 
-
-        public void CheckPlateNumber(int bingoNumber, BankoPlade plade)
+        // Method to check a bingo number on the plate and update status
+        public void CheckAndInsertPlateNumber(int bingoNumber, BankoPlade plade)
         {
             int bingoNumberIndex = bingoNumber / 10;
             if (bingoNumberIndex == 9)
                 bingoNumberIndex--;
 
-            if (!Row1.lineStatus && Row1.numbers[bingoNumberIndex] == bingoNumber)
-                Row1.InsertBingoNumberIntoRow(Row1, bingoNumberIndex, 1, plade);
-            else if (!Row2.lineStatus && Row2.numbers[bingoNumberIndex] == bingoNumber)
-                Row2.InsertBingoNumberIntoRow(Row2, bingoNumberIndex, 2, plade);
-            else if (!Row3.lineStatus && Row3.numbers[bingoNumberIndex] == bingoNumber)
-                Row3.InsertBingoNumberIntoRow(Row3, bingoNumberIndex, 3, plade);
+            if (!row1.lineStatus && row1.numbers[bingoNumberIndex] == bingoNumber)
+                row1.InsertBingoNumberIntoRow(row1, bingoNumberIndex, 1, plade);
+            else if (!row2.lineStatus && row2.numbers[bingoNumberIndex] == bingoNumber)
+                row2.InsertBingoNumberIntoRow(row2, bingoNumberIndex, 2, plade);
+            else if (!row3.lineStatus && row3.numbers[bingoNumberIndex] == bingoNumber)
+                row3.InsertBingoNumberIntoRow(row3, bingoNumberIndex, 3, plade);
         }
 
+        // Method to get a random bingo number
         public int GetBingoNumber()
         {
             Random r = new();
             return r.Next(1, 91);
         }
 
+        // Method to create rows for the bingo plate
         public void CreateRows()
         {
             List<int> usedNumbers = new();
-            Row1.CreateRow(usedNumbers);
-            Row2.CreateRow(usedNumbers);
-            Row3.CreateRow(usedNumbers);
+            row1.CreateRow(usedNumbers);
+            row2.CreateRow(usedNumbers);
+            row3.CreateRow(usedNumbers);
         }
 
+        // Method to print the bingo plate
         public void PrintPlade()
         {
             Console.WriteLine($"{name}:");
-            PrintRow(Row1);
-            PrintRow(Row2);
-            PrintRow(Row3);
+            PrintRow(row1);
+            PrintRow(row2);
+            PrintRow(row3);
         }
 
+        // Helper method to print a row
         private void PrintRow(Row row)
         {
             foreach (int elm in row.numbers)
@@ -284,24 +379,29 @@ class Program
             Console.WriteLine();
         }
 
+        // Method to set the name of the plate
         public void SetName(string passedName)
         {
             name = passedName;
         }
     }
 
+    // Class representing a row in a bingo plate
     public class Row
     {
-        public int[] numbers { get; set; } = new int[9];
-        public int points { get; set; } = 0;
-        public bool lineStatus { get; set; } = false;
+        // Properties of a row
+        public int[] numbers { get; set; } = new int[9]; // Numbers in the row
+        public int points { get; set; } = 0; // Points earned in the row
+        public bool lineStatus { get; set; } = false; // Status of the line (completed or not)
 
-        // The full creation of a row.
+        // Method to create a row with random numbers
         public void CreateRow(List<int> usedNumbers)
         {
             int[] indexes = getIndexies(); // Where the numbers will be placed.
             numbers = GetNumbers(indexes, usedNumbers); // What numbers will be placed.
         }
+
+        // Method to examine the row for errors (number of empty spots)
         public int ExamineRow()
         {
             int zeroCounter = 0;
@@ -315,6 +415,7 @@ class Program
             return zeroCounter;
         }
 
+        // Method to insert a bingo number into the row
         public void InsertBingoNumberIntoRow(Row row, int bingoNumberIndex, int rowNum, BankoPlade plade)
         {
             row.numbers[bingoNumberIndex] = 99;
@@ -333,6 +434,7 @@ class Program
 
         }
 
+        // Helper method to get numbers for the row
         private int[] GetNumbers(int[] indexes, List<int> usedNumbers)
         {
             int number;
@@ -365,6 +467,7 @@ class Program
             return row;
         }
 
+        // Helper method to check for duplicate numbers
         private bool CheckDuplicateNumber(List<int> usedNumbers, int number)
         {
             if (usedNumbers.Contains(number))
@@ -373,6 +476,7 @@ class Program
                 return false;
         }
 
+        // Helper method to get indexes for number placement in the row
         private int[] getIndexies()
         {
             int[] indexes = new int[5]; // Indexes where our values will be placed.
@@ -392,8 +496,7 @@ class Program
 
     static void Main()
     {
-        Hashtable ht = new Hashtable();
-        BingoBankoSpil t = new(1);
-        ht.Add("1", t);
+        AlternativeBingo test = new();
+        test.StartGame();
     }
 }
--- andreas_banko/Banko/obj/Debug/net8.0/Banko.AssemblyInfo.cs
+++ andreas_banko/Banko/obj/Debug/net8.0/Banko.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Banko")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77bece8e8b9974b4f66d48907abc9f4b389c6114")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ec179370d125bf6e2f188ff292fb093d8f298ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("Banko")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Banko")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
